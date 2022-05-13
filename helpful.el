a0a687546aa0eca89f38f02c847992c75cbc4031
Kisaragi Hiu <mail@kisaragi-hiu.com>
2022-05-14 07:36:13 +0900

    Move the admin block back into Documentation
    I find this more appealing as the "Administration" header really
    doesn't make a lot of sense. (Perhaps "Miscellaneous" would work?)

--- helpful.el
+++ helpful.el
@@ -2313,8 +2313,9 @@ state of the current symbol."
 
     (let ((docstring (helpful--docstring helpful--sym helpful--callable-p))
           (version-info (unless helpful--callable-p
-                          (helpful--version-info helpful--sym))))
-      (when (or docstring version-info)
+                          (helpful--version-info helpful--sym)))
+          (admin-block (helpful--admin-block helpful--sym helpful--callable-p)))
+      (when (or docstring version-info admin-block)
         (helpful--insert-section-break)
         (insert
          (helpful--heading "Documentation"))
@@ -2322,17 +2323,12 @@ state of the current symbol."
           (insert (helpful--format-docstring docstring)))
         (when version-info
           (insert "\n\n" (s-word-wrap 70 version-info)))
+        ;; Not sure if this should be in Documentation or its own section.
+        (when admin-block
+          (insert "\n\n" (s-join "\n" admin-block)))
         (when (and (symbolp helpful--sym) (helpful--in-manual-p helpful--sym))
           (insert "\n\n")
           (insert (helpful--make-manual-button helpful--sym)))))
-
-    (when (and (symbolp helpful--sym))
-      (-when-let (help-fns-output
-                  (helpful--admin-block helpful--sym helpful--callable-p))
-        (helpful--insert-section-break)
-        (insert
-         (helpful--heading "Administrative")
-         help-fns-output)))
 
     ;; Show keybindings.
     ;; TODO: allow users to conveniently add and remove keybindings.
@@ -2584,8 +2580,7 @@ and \"other relevant functions\" section in `describe-function'."
           (unless (= 0 (buffer-size))
             (push (s-trim (buffer-string))
                   ret))))
-      (when ret
-        (s-join "\n" (nreverse ret))))))
+      (nreverse ret))))
 
 ;; TODO: this is broken for -any?.
 (defun helpful--signature (sym)
